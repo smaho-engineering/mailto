b5b8054a771cd41fd66bb768291877c006a572f2
Vince Varga <vince.varga@smaho.com>
2020-02-03 12:13:32 +0100

    Add Flutter example app

--- example/flutter/README.md
+++ example/flutter/README.md
@@ -1,16 +1,11 @@
 # mailto_example
 
-A new Flutter project.
+This is a Flutter example app of the [`mailto` package](https://pub.dev/packages/mailto).
 
-## Getting Started
+Work in progress.
 
-This project is a starting point for a Flutter application.
+To-dos:
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+* The sections' leading icons should change from filled to outlined depending on whether the section is enabled.
+* Make sure users can hide their keyboard.
+* Add pictures and video of the example app
--- example/flutter/lib/main.dart
+++ example/flutter/lib/main.dart
@@ -1,111 +1,406 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:mailto/mailto.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MailtoExampleApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MailtoExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(
+        textTheme: CupertinoTextThemeData(
+          navLargeTitleTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+            color: CupertinoColors.activeGreen,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: MailtoDemo(),
     );
+  }
+}
+
+const Color white = Color(0xFFFFFFFF);
+const EdgeInsets textFieldPadding = EdgeInsets.all(16);
+const EdgeInsets iconPadding = EdgeInsets.fromLTRB(12, 0, 12, 0);
+
+class MailtoDemo extends StatefulWidget {
+  @override
+  _MailtoDemoState createState() => _MailtoDemoState();
+}
+
+class _MailtoDemoState extends State<MailtoDemo> {
+  List<String> to = [];
+  List<String> cc = [];
+  List<String> bcc = [];
+  String subject = '';
+  String body = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: white,
+        middle: Text(
+          'mailto',
+        ),
+      ),
+      child: ListView(
+        children: <Widget>[
+          EmailsContainer(
+            onChanged: (v) => to = v,
+            icon: CupertinoIcons.person_solid,
+            title: 'to',
+            placeholder: 'Recipient',
+          ),
+          EmailsContainer(
+            onChanged: (v) => cc = v,
+            icon: CupertinoIcons.group_solid,
+            title: 'cc',
+            placeholder: 'Carbon-copy',
+          ),
+          EmailsContainer(
+            onChanged: (v) => bcc = v,
+            icon: CupertinoIcons.group,
+            title: 'bcc',
+            placeholder: 'Blind Carbon-copy',
+          ),
+          SubjectTextField(
+            onChanged: (v) => subject = v,
+          ),
+          BodyTextField(
+            onChanged: (v) => body = v,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 32,
+              horizontal: 12,
+            ),
+            child: CupertinoButton.filled(
+              child: Text('Open Mail Client'),
+              onPressed: () async {
+                final url = Mailto(
+                  to: to,
+                  cc: cc,
+                  bcc: bcc,
+                  subject: subject,
+                  body: body,
+                ).toString();
+
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: MailClientOpenErrorDialog(url: url).build,
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MailClientOpenErrorDialog extends StatelessWidget {
+  final String url;
+
+  const MailClientOpenErrorDialog({Key key, @required this.url})
+      : assert(url != null),
+        assert(url != ''),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text('Launch Error'),
+      content: Text('We could not launch the following url:\n$url'),
+      actions: <Widget>[
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          child: Text('OK'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class BodyTextField extends StatefulWidget {
+  final ValueChanged<String> onChanged;
+
+  const BodyTextField({
+    Key key,
+    @required this.onChanged,
+  })  : assert(onChanged != null),
+        super(key: key);
+
+  @override
+  _BodyTextFieldState createState() => _BodyTextFieldState();
+}
+
+class _BodyTextFieldState extends State<BodyTextField> {
+  final TextEditingController _controller = TextEditingController();
+
+  bool isEnabled = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SectionHeading(
+          leadingIcon: CupertinoIcons.book,
+          title: 'body',
+          trailingIcon:
+              isEnabled ? CupertinoIcons.delete : CupertinoIcons.add_circled,
+          onPressed: () {
+            setState(() => isEnabled = !isEnabled);
+            _controller.text = '';
+            if (isEnabled) {
+              widget.onChanged(_controller.text);
+            } else {
+              widget.onChanged(null);
+            }
+          },
+        ),
+        if (isEnabled)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+            child: CupertinoTextField(
+              controller: _controller,
+              padding: textFieldPadding,
+              minLines: 4,
+              maxLines: null,
+              textCapitalization: TextCapitalization.sentences,
+              keyboardType: TextInputType.multiline,
+              placeholder: 'Body of the email',
+              onChanged: widget.onChanged,
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class SectionHeading extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+  final IconData leadingIcon;
+  final IconData trailingIcon;
+
+  const SectionHeading({
+    Key key,
+    @required this.title,
+    @required this.onPressed,
+    @required this.leadingIcon,
+    @required this.trailingIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: iconPadding,
+                child: Icon(leadingIcon),
+              ),
+              LargeText(title),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: CupertinoButton(
+              child: Icon(trailingIcon),
+              onPressed: onPressed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SubjectTextField extends StatefulWidget {
+  final ValueChanged<String> onChanged;
+
+  const SubjectTextField({
+    Key key,
+    @required this.onChanged,
+  })  : assert(onChanged != null),
+        super(key: key);
+
+  @override
+  _SubjectTextFieldState createState() => _SubjectTextFieldState();
+}
+
+class _SubjectTextFieldState extends State<SubjectTextField> {
+  final TextEditingController _controller = TextEditingController();
+
+  bool isEnabled = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SectionHeading(
+          leadingIcon: CupertinoIcons.mail,
+          title: 'subject',
+          trailingIcon:
+              isEnabled ? CupertinoIcons.delete : CupertinoIcons.add_circled,
+          onPressed: () {
+            setState(() => isEnabled = !isEnabled);
+            _controller.text = '';
+            if (isEnabled) {
+              widget.onChanged(_controller.text);
+            } else {
+              widget.onChanged(null);
+            }
+          },
+        ),
+        if (isEnabled)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+            child: CupertinoTextField(
+              controller: _controller,
+              padding: textFieldPadding,
+              textCapitalization: TextCapitalization.words,
+              placeholder: 'Subject of the email',
+              onChanged: widget.onChanged,
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class LargeText extends StatelessWidget {
+  final String data;
+
+  const LargeText(this.data, {Key key})
+      : assert(data != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Text(
+        data,
+        style: CupertinoTheme.of(context)
+            .textTheme
+            .navLargeTitleTextStyle
+            .copyWith(color: CupertinoColors.black),
+      ),
+    );
+  }
+}
+
+class EmailsContainer extends StatefulWidget {
+  const EmailsContainer({
+    Key key,
+    @required this.title,
+    @required this.icon,
+    @required this.onChanged,
+    @required this.placeholder,
+  }) : super(key: key);
+
+  final String title;
+  final String placeholder;
+  final IconData icon;
+  final ValueChanged<List<String>> onChanged;
+
+  @override
+  _EmailsContainerState createState() => _EmailsContainerState();
+}
+
+class _EmailsContainerState extends State<EmailsContainer> {
+  final List<TextEditingController> _controllers = [];
+
+  @override
+  void dispose() {
+    _controllers.forEach((c) => c.dispose());
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SectionHeading(
+          leadingIcon: widget.icon,
+          trailingIcon: CupertinoIcons.plus_circled,
+          title: widget.title,
+          onPressed: () {
+            setState(() {
+              _controllers.add(TextEditingController(text: ''));
+            });
+            callOnChanged();
+          },
+        ),
+        ...buildTextFields(),
+      ],
+    );
+  }
+
+  void callOnChanged() {
+    widget.onChanged(_controllers.map((c) => c.text.trim()).toList());
+  }
+
+  void removeController(int index) {
+    assert(index >= 0);
+    assert(index < _controllers.length);
+    setState(() => _controllers.removeAt(index));
+    callOnChanged();
+  }
+
+  List<Widget> buildTextFields() {
+    final widgets = <Widget>[];
+    _controllers.asMap().forEach((index, controller) {
+      widgets.add(Padding(
+        padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+        child: CupertinoTextField(
+          keyboardType: TextInputType.emailAddress,
+          controller: controller,
+          padding: textFieldPadding,
+          suffix: CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () => removeController(index),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+              child: Icon(CupertinoIcons.minus_circled),
+            ),
+          ),
+          textCapitalization: TextCapitalization.none,
+          placeholder: '${widget.placeholder} ${index + 1}',
+          onChanged: (_) => callOnChanged(),
+        ),
+      ));
+    });
+    return widgets;
   }
 }
